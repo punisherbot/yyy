81b94797117680957bbe9513d245d90e1bf0a695
ArashTAQS <arash.wilson.am@gmail.com>
2016-01-12 07:25:40 -0800

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,19 +1,19 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
-   -- like local bot_id = 1234567
-    if matches[1] == 'leave' and is_admin(msg) then
+local bot_id = 177958856 -- your bot id
+   -- like local bot_id = 109722284
+    if matches[1] == 'left' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
+       send_large_msg("chat#id"..msg.to.id, 'Alan babam miyad kunet mizare.', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leave)$",
+    "^[!/](left)$",
     "^!!tgservice (.+)$",
   },
   run = run
