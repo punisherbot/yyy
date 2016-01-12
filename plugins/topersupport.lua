ebb04f479a0e0e8921123ce74dd29df3825ec77e
ArashTAQS <arash.wilson.am@gmail.com>
2016-01-12 07:56:03 -0800

    Update topersupport.lua

--- plugins/topersupport.lua
+++ plugins/topersupport.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return " The Invition for Infernals Persian Support is : \n "
+  return " The Invition for Infernals Persian Support is : \nhttps://telegram.me/joinchat/CptvyAXK-WfzNZF9gjHn3Q "
   end
 return {
   description = "shows persian supports grouplink", 
