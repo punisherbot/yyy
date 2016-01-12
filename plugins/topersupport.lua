116234c199588b5db76b59bcf12b462f7eb21884
ArashTAQS <arash.wilson.am@gmail.com>
2016-01-12 07:43:05 -0800

    Create topersupport.lua

---
+++ plugins/topersupport.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " The Invition for Infernals Persian Support is : \n "
+  end
+return {
+  description = "shows persian supports grouplink", 
+  usage = "!topersupport : ",
+  patterns = {
+    "^topersupport$",
+    "^/topersupport$",
+    "^!topersupport$",
+    "^>topersupport$",
+  },
+  run = run
+}
+end
