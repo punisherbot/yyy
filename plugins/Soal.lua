b479d9427c1ff9882bf6b950b9b670f580c9e7a5
ArashTAQS <arash.wilson.am@gmail.com>
2016-01-12 07:30:39 -0800

    Create Soal.lua

---
+++ plugins/Soal.lua
@@ -0,0 +1,16 @@
+do
+
+function run(msg, matches)
+  return " زياد سوال ميکنيــــــــــــــــــــا "
+  end
+return {
+  description = "infernal bot about", 
+  usage = "infernal : infernal about",
+  patterns = {
+    "^?$",
+    "^????$",
+    "^Arash?$",
+  },
+  run = run
+}
+end
