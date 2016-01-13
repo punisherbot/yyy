8d4786a44e5c80d146d84f19d1e6886f0a8b2e22
Arash-SiS <arash.wilson.am@gmail.com>
2016-01-13 13:03:56 -0800

    Update SUDO.lua

--- plugins/SUDO.lua
+++ plugins/SUDO.lua
@@ -1,7 +1,13 @@
 do
 
 function run(msg, matches)
-  return " 👥Sudoers of Infernal :\n🔭 Master sudoer : @ArashInfernal 109722284 \n🔭 @Mustafaflux 171604508 👥\n🔭 @digitalboys  99743635 👥 "
+  return [[
+  👥Sudoers of Infernal : 
+  🔭 @ArashInfernal 109722284👥
+  🔭 @Mustafaflux 171604508 👥 
+  🔭 @digitalboys  99743635 👥
+  ]]
+
   end
 return {
   description = "shows sudoers", 
