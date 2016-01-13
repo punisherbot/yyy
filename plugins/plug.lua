120059604016b9ed954a74c17b341265894fb3c2
Arash-SiS <arash.wilson.am@gmail.com>
2016-01-13 13:53:54 -0800

    Update plug.lua

--- plugins/plug.lua
+++ plugins/plug.lua
@@ -152,7 +152,7 @@ end
 
 local function run(msg, matches)
   -- Show the available plugins 
-  if matches[1] == 'plug' and is_sudo(msg) then --after changed to moderator mode, set only sudo
+  if matches[1] == 'pluglist' and is_sudo(msg) then --after changed to moderator mode, set only sudo
     return list_all_plugins()
   end
 
@@ -208,7 +208,7 @@ return {
           "plug * : reloads all plugins." },
           },
   patterns = {
-    "^[/!]plug$",
+    "^[/!]pluglist$",
     "^[/!]plug? (+) ([%w_%.%-]+)$",
     "^[/!]plug? (-) ([%w_%.%-]+)$",
     "^[/!]plug? (+) ([%w_%.%-]+) (gp)",
