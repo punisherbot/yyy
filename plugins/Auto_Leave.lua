6b43c4a19d57e55d64f5f1097bf1a886f12d3e22
Arash-SiS <arash.wilson.am@gmail.com>
2016-01-14 11:03:56 -0800

    Update Auto_Leave.lua

--- plugins/Auto_Leave.lua
+++ plugins/Auto_Leave.lua
@@ -2,11 +2,11 @@ do
 -- Will leave the group if be added
 local function run(msg, matches)
 local bot_id = 177958856 -- your bot id
-   -- like local bot_id = 177958856
+   -- like local bot_id = 1234567
     if matches[1] == 'left' and is_sudo(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-       send_large_msg("chat#id"..msg.to.id, 'Kooon bache baaaz ke addam kardi!.', ok_cb, false)
+       send_large_msg("chat#id"..msg.to.id, 'یعنی مردم از خنده کیر شدی گوساله گاو کیرم دهنت کونی گاو بگیرم بگامت؟کونی سگ پدر کس پدر', ok_cb, false)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     end
 end
@@ -19,3 +19,4 @@ return {
   run = run
 }
 end
+
