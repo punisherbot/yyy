d61b7b9165f4139e9ef7fe6bf65f5b88250e82af
Arash-SiS <arash.wilson.am@gmail.com>
2016-01-13 17:33:21 -0800

    Create Version.lua

---
+++ plugins/Version.lua
@@ -0,0 +1,30 @@
+do
+
+function run(msg, matches)
+return [[ 
+INFERNALTEAM Robot Ver 4.2
+AntispamBot : @InfernalTG
+website : https://arashinfernal.epage.ir
+Our Channel : @INFERNALTEAMCH
+@INFERNALCHANNEL
+@INFERNALTEAM
+Our code is open source but !
+remember : copy right!
+https://github.com/INFERNALTEAM/InfernalTG
+spical thanx...
+imandaneshi ...
+iwals ...
+
+]]
+end
+
+return {
+description = "Shows bot Version", 
+usage = "Version  : Shows bot Version",
+patterns = {
+"^!version$"
+}, 
+run = run 
+}
+
+end
