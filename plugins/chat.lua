4cf94a017e4189c0afdfca46c60682ca0e80c5fc
Arash-SiS <arash.wilson.am@gmail.com>
2016-01-14 10:58:48 -0800

    Create chat.lua

---
+++ plugins/chat.lua
@@ -0,0 +1,69 @@
+local function run(msg)
+if msg.text == "hi" then
+	return "Hello bb"
+end
+if msg.text == "Hi" then
+	return "Hello honey"
+end
+if msg.text == "Hello" then
+	return "Hi bb"
+end
+if msg.text == "hello" then
+	return "Hi honey"
+end
+if msg.text == "Salam" then
+	return "Salam aleykom"
+end
+if msg.text == "salam" then
+	return "و علیکم السلام"
+end
+if msg.text == "InfernalTG" then    
+	return "BEst Bot In The World!"
+end
+if msg.text == "INFERNALtg" then
+	return "Best Bot In The World!"
+end
+if msg.text == "INFERNALTG" then
+	return "Best Bot In The World"
+end
+if msg.text == "Infernal" then
+	return "Yes?"
+end
+if msg.text == "infernal" then
+	return "What?"
+end
+if msg.text == "bot" then
+	return "hum?"
+end
+if msg.text == "Bot" then
+	return "Huuuum?"
+end
+if msg.text == "?" then
+	return "Hum??"
+end
+if msg.text == "Bye" then
+	return "Babay"
+end
+if msg.text == "bye" then
+	return "Bye Bye"
+end
+end
+
+return {
+	description = "Chat With Robot Server", 
+	usage = "chat with robot",
+	patterns = {
+		"^[Hh]i$",
+		"^[Hh]ello$",
+		"^[Zz]ac$",
+		"^InfernalTG$",
+		"^[Bb]ot$",
+		"^[Ii]nfernal$",
+		"^[Bb]ye$",
+		"^?$",
+		"^[Ss]alam$",
+		}, 
+	run = run,
+    --privileged = true,
+	pre_process = pre_process
+}
