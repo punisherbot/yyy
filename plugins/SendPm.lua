5bf6c2cb365ac3abc6a184fb7dadaf6758e79165
Arash-SiS <arash.wilson.am@gmail.com>
2016-01-14 06:54:35 +0330

    Create SendPm.lua

---
+++ plugins/SendPm.lua
@@ -0,0 +1,17 @@
+local function run(msg, matches)
+  if not is_sudo(msg) then -- For admins only !
+    return 'ًsudo zone!'
+  end
+	if matches[1] == 'pv' and is_admin(msg) then
+		local response = matches[3]
+		send_large_msg("user#id"..matches[2], response)
+						local receiver = 'user#id'..user_id
+				send_large_msg(receiver, response)
+	end
+	end
+return {
+  patterns = {
+    "^[!/](SendPm) (%d+) (.*)$"
+  },
+  run = run
+}
